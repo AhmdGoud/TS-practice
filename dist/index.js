c3316155de89206e8124e8d2ba20cdd6922c1bf3
ahmed gouda <ag021055@gmail.com>
2026-07-17 08:13:02 +0300

    type annotations with functions

--- dist/index.js
+++ dist/index.js
@@ -17,10 +17,6 @@ depends = 100;
 // depends = true; // error cause we didnt mention it can be boolean
 // also we can declare it with value
 let all = "all";
-function add(n1, n2) {
-    return n1 + n2;
-}
-console.log(add(5, "15"));
 const myArr1 = ["abc", "def", "ghk"];
 // in this arr if we pushed a number we will get error
 // const myArr2: string[] | number[] = ["abc", "def", "ghk", 10];
@@ -30,4 +26,25 @@ const myArr3 = ["abc", "def", "ghk", 10];
 // multidimensional arrays
 const myArr4 = ["abc", "def", "ghk", 10, 20, ["sfh", 30, ["skd", 40]]];
 // this is 3 arr in
+// the string after add() is a type annotation for the function output
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(5, "15"));
+// if we changed the n2 type to be number we will get an error
+// as the output of the function will be a number not a string
+// ? is for optional parameters and it's must be in the last after required parameters
+function sum(n1 = 1, n2) {
+    return `${n1}, ${n2}`;
+}
+console.log(sum(undefined));
+// we passed undefined the compiler will take the default value for n1 and n2 is optional
+const addNums = function (...nums) {
+    let result = 0;
+    nums.forEach((num) => (result += num));
+    return result;
+};
+console.log(addNums(3, 4, 5.5, +true));
+const addNumsArrow = (n1, n2) => n1 + n2;
+console.log(addNumsArrow(1, 2));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,6 @@ depends = 100;
 // also we can declare it with value
 let all: string | number = "all";
 
-function add(n1: number, n2: string) {
-  return n1 + n2;
-}
-console.log(add(5, "15"));
-
 const myArr1: string[] = ["abc", "def", "ghk"];
 // in this arr if we pushed a number we will get error
 
@@ -37,3 +32,28 @@ const myArr3: (string | number)[] = ["abc", "def", "ghk", 10];
 const myArr4: (string | number | (string | number | (string | number)[])[])[] =
   ["abc", "def", "ghk", 10, 20, ["sfh", 30, ["skd", 40]]];
 // this is 3 arr in
+
+// the string after add() is a type annotation for the function output
+function add(n1: number, n2: string): string {
+  return n1 + n2;
+}
+console.log(add(5, "15"));
+// if we changed the n2 type to be number we will get an error
+// as the output of the function will be a number not a string
+
+// ? is for optional parameters and it's must be in the last after required parameters
+function sum(n1: number = 1, n2?: number) {
+  return `${n1}, ${n2}`;
+}
+console.log(sum(undefined));
+// we passed undefined the compiler will take the default value for n1 and n2 is optional
+
+const addNums = function (...nums: number[]): number {
+  let result = 0;
+  nums.forEach((num) => (result += num));
+  return result;
+};
+console.log(addNums(3, 4, 5.5, +true));
+
+const addNumsArrow = (n1: number, n2: number): number => n1 + n2;
+console.log(addNumsArrow(1, 2));
